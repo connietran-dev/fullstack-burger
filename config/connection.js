155a1ca639cd9474b2c1b988b319d59949fc7ebb
Connie Tran <connietrandev@gmail.com>
2020-05-27 22:58:05 -0400

    Adjust db connection for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Set up connection from Node to MySQL and export the connection
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_DB"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_DB"
+  })
+};
 
 // Make connection
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
